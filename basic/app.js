936e3416f82246811c7db584fe0f36a9f318d774
LeeJams <woals214@gmail.com>
2022-09-28 18:00:24 +0900

    기본 데이터 받는 법

--- basic/app.js
+++ basic/app.js
@@ -1,10 +1,36 @@
 const http = require("http");
+const fs = require("fs");
 
 function rqListener(req, res) {
-  console.log(req.url);
-  console.log(req.method);
-  console.log(req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
+  if (url === "/message" && method === "POST") {
+    // 데이터 스트림
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody)
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
