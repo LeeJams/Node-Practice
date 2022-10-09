314b953088bd9b3d86b3aa27e275601cc5e355ce
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-10-10 00:06:51 +0900

    express 기초 와

--- express/app.js
+++ express/app.js
@@ -8,6 +8,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+// .css or .js 를 찾는 요청이면 자동으로 public폴더로 포워딩 해줌
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
