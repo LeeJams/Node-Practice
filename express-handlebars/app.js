da0a190581fcc45d104e0f3a2bb735276b3dbabe
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-10-12 23:30:29 +0900

    수정

--- express-handlebars/app.js
+++ express-handlebars/app.js
@@ -1,24 +1,33 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// .css or .js 를 찾는 요청이면 자동으로 public폴더로 포워딩 해줌
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { docTitle: "Page Not Found" });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
