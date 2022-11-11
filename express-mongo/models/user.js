cc452b6d8d9567d618de8ba265015600da76bb5c
LeeJams <woals214@gmail.com>
2022-11-11 22:04:37 +0900

    mongo

--- express-mongo/models/user.js
+++ express-mongo/models/user.js
@@ -1,16 +1,28 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
+class User {
+  constructor(username, email, id) {
+    this.name = username;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then((result) => {
+        console.log("User Created!");
+      })
+      .catch((err) => console.log(err));
+  }
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then((user) => user)
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = User;
